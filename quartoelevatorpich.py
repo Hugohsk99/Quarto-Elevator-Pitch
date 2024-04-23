44000a9100f60932e180db56960948a1af357df1
Hugo Rosa <hugo.libresolucoes@gmail.com>
2024-04-23 18:54:13 -0300

    Inicio, resumindo o solicitado

---
+++ quartoelevatorpich.py
@@ -0,0 +1,5 @@
+# A explicação sobre o impacto da esparsidade dos dados de qualidade da água e estratégias para otimização da busca em dados esparsos.
+# O desenvolvimento de um algoritmo teórico que utilize uma estrutura de dados especializada para minimizar o acesso a dados nulos e acelerar a localização de registros válidos.
+# Implementação de uma técnica para minimização de acessos nulos durante a busca.
+# Análise da complexidade temporal do algoritmo otimizado em comparação com uma busca linear simples e uma pesquisa binária, além de condições de preferência para quando o algoritmo otimizado é a melhor escolha.
+# Codificação do algoritmo em Python e visualização dos dados e da busca.
